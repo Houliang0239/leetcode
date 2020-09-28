3085efb3e02a840f9f3b7abd726e29bd02695aed
houliang <houliang0239@foxmail.com>
2020-09-28 19:51:30 +0800

    20200928

---
+++ stack/682.js
@@ -0,0 +1,27 @@
+function calPoints(ops) {
+    let ans = 0
+    let preScore = []
+    for (let i = 0; i < ops.length; i++) {
+        switch (ops[i]) {
+            case 'C':
+                ans -= preScore.pop()
+                break
+            case 'D':
+                ans += preScore[preScore.length - 1] * 2
+                preScore.push(preScore[preScore.length - 1] * 2)
+                break
+            case '+':
+                ans += (preScore[preScore.length - 1] + preScore[preScore.length - 2])
+                preScore.push(preScore[preScore.length - 1] + preScore[preScore.length - 2])
+                break
+            default:
+                preScore.push(parseInt(ops[i]))
+                ans += parseInt(ops[i])
+                break
+        }
+    }
+    console.log(ans)
+    return ans
+}
+let ops =  ["5","-2","4","C","D","9","+","+"]
+calPoints(ops)
